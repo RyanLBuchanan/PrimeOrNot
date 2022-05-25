14c59a15f76d0a84b763bb1a003031049fb6647b
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-05-25 05:58:09 -0600

    Add project files.

---
+++ PrimeOrNot/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PrimeOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num, i, counter = 0;
+
+            Console.Write("\n\n");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
