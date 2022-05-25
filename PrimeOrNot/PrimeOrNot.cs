d38bbe3aeb246f68ad648f69550503c6e0f9d621
Ryan Buchanan <buchanan.ryan22@gmail.com>
2022-05-25 08:34:09 -0600

    feat: Completed Prime number app

--- PrimeOrNot/PrimeOrNot.cs
+++ PrimeOrNot/PrimeOrNot.cs
@@ -2,7 +2,7 @@
 
 namespace PrimeOrNot
 {
-    class Program
+    class PrimeOrNot
     {
         static void Main(string[] args)
         {
@@ -18,7 +18,7 @@ namespace PrimeOrNot
             num = Convert.ToInt32(Console.ReadLine());
             for (i = 2; i <= num / 2; i++)
             {
-                if(num % i == 0)
+                if (num % i == 0)
                 {
                     counter++;
                     break;
@@ -27,11 +27,11 @@ namespace PrimeOrNot
 
             if (counter == 0 && num != 1)
             {
-                Console.Write($"{0} is a prime number.\n", num);
+                Console.Write($"{num} is a prime number.\n", num);
             }
             else
             {
-                Console.Write($"{0} is a prime number.\n", num);
+                Console.Write($"{num} is not a prime number.\n", num);
             }
         }
     }
