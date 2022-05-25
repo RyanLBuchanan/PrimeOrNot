ea51950970e9d9a7fd9fc575c9224a5573dc6eab
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-05-25 06:13:08 -0600

    feat: Completed PrimeOrNot solution

--- PrimeOrNot/Program.cs
+++ PrimeOrNot/Program.cs
@@ -10,8 +10,29 @@ namespace PrimeOrNot
             int num, i, counter = 0;
 
             Console.Write("\n\n");
+            Console.Write("Check whether a given number is prime or not:\n");
+            Console.Write("-----------------------------------------------");
+            Console.Write("\n\n");
 
-            Console.WriteLine("Hello World!");
+            Console.Write("Input a number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (i = 2; i <= num / 2; i++)
+            {
+                if(num % i == 0)
+                {
+                    counter++;
+                    break;
+                }
+            }
+
+            if (counter == 0 && num != 1)
+            {
+                Console.Write($"{0} is a prime number.\n", num);
+            }
+            else
+            {
+                Console.Write($"{0} is a prime number.\n", num);
+            }
         }
     }
 }
